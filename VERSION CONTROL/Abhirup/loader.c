e76be03c7bc900b534d707dae5ef61f1a08721b0
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-08-19 17:21:11 +0530

    Loader.c Part 2 Written

--- VERSION CONTROL/Abhirup/loader.c
+++ VERSION CONTROL/Abhirup/loader.c
@@ -16,6 +16,7 @@ void loader_cleanup() {
  */
 void load_and_run_elf(char** exe) {
   fd = open(argv[1], O_RDONLY);
+
   // 1. Load entire binary content into the memory from the ELF file.
 
   ehdr = (Elf32_Ehdr*)malloc((size_t)52);
@@ -26,11 +27,25 @@ void load_and_run_elf(char** exe) {
 
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
   //    type that contains the address of the entrypoint method in fib.c
+
+  Elf32_Phdr* Req_Prog_Header;
+
+  for(int i = 0; i < ehdr->e_phnum; i++){
+    if(phdr->p_type == PT_LOAD && phdr->p_vaddr == ehdr->e_entry){
+        Req_Prog_Header = phdr;
+        break;
+    }
+    else{
+        phdr += ehdr->e_phentsize;
+    }
+  }
+
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
   // 6. Call the "_start" method and print the value returned from the "_start"
+
   int result = _start();
   printf("User _start return value = %d\n",result);
 }
