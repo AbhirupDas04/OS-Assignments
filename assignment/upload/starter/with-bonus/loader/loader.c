44fdd52a424436ba922dfd8bc5f78bb46588599e
Armaan Singh <123837862+Aturtle-4@users.noreply.github.com>
2023-08-22 08:30:18 +0000

    aunt-IV: Rise of the fallen aunt

--- assignment/upload/starter/with-bonus/loader/loader.c
+++ assignment/upload/starter/with-bonus/loader/loader.c
@@ -8,22 +8,70 @@ int fd;
  * release memory and other cleanups
  */
 void loader_cleanup() {
-  
+  free(ehdr);
+  free(phdr);
 }
 
 /*
  * Load and run the ELF executable file
  */
 void load_and_run_elf(char** exe) {
-  fd = open(argv[1], O_RDONLY);
+    fd = open(*exe, O_RDONLY);
   // 1. Load entire binary content into the memory from the ELF file.
+
+  ehdr = (Elf32_Ehdr*)malloc((size_t)52);
+  ssize_t NBytes_Read_EHDR = read(fd, ehdr, (size_t)52);
+  if(NBytes_Read_EHDR == -1){
+    printf("Error in Reading EHDR");
+    exit(1);
+  }
+
+  phdr = (Elf32_Phdr*)malloc((size_t)(ehdr->e_phentsize * ehdr->e_phnum));
+  ssize_t NBytes_Read_PHDR = read(fd, phdr, (size_t)(ehdr->e_phentsize * ehdr->e_phnum));
+  if(NBytes_Read_PHDR == -1){
+    printf("Error in Reading PHDR");
+    exit(1);
+  }
+
   // 2. Iterate through the PHDR table and find the section of PT_LOAD 
   //    type that contains the address of the entrypoint method in fib.c
+
+  Elf32_Phdr Req_Prog_Header;
+  int index;
+
+  for(int i = 0; i < ehdr->e_phnum; i++){
+    if(phdr[i].p_type == PT_LOAD && phdr[i].p_vaddr < ehdr->e_entry && phdr[i].p_vaddr + phdr[i].p_memsz > ehdr->e_entry){
+        Req_Prog_Header = phdr[i];
+        index = i;
+        break;
+    }
+  }
+
   // 3. Allocate memory of the size "p_memsz" using mmap function 
   //    and then copy the segment content
+
+  void* virtual_mem = mmap(NULL, Req_Prog_Header.p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0,0);
+  if(virtual_mem == (void*)(-1)){
+    printf("MMAP ERROR");
+    exit(1);
+  }
+
+  lseek(fd,phdr[index].p_offset,SEEK_SET);
+  read(fd, virtual_mem,phdr[index].p_memsz);
+
   // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+
+  int address = ((int) virtual_mem + (ehdr->e_entry - phdr[index].p_vaddr));
+
   // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+
+  typedef int(*fnc)();
+  fnc _start = (fnc)(int)address;
+
   // 6. Call the "_start" method and print the value returned from the "_start"
+
   int result = _start();
   printf("User _start return value = %d\n",result);
+
+  munmap(virtual_mem,Req_Prog_Header.p_memsz);
 }
