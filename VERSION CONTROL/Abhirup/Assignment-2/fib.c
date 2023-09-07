a29c1dcaeec5ef05e1834c0f80afa1328717936e
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-09-07 21:44:44 +0530

    Update fib.c

--- VERSION CONTROL/Abhirup/Assignment-2/fib.c
+++ VERSION CONTROL/Abhirup/Assignment-2/fib.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int fib(int n) {
   if(n<2) return n;
   else return fib(n-1)+fib(n-2);
 }
 
-int main() {
-	int val = fib(40);
-	return val;
+void main(int argc, char *argv[]) {
+  int n = atoi(argv[1]);
+	int val = fib(n);
+  printf("%d\n",val);
 }
