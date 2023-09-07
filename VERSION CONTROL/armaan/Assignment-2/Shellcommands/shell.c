9f184a74e90aa854e9ef6e929ac1e3cc9f7add1f
Armaan Singh <123837862+Aturtle-4@users.noreply.github.com>
2023-09-07 10:47:49 +0000

    made changes after realising

---
+++ VERSION CONTROL/armaan/Assignment-2/Shellcommands/shell.c
@@ -0,0 +1,86 @@
+#include "shell.h"
+//PATH_MAX wasnt working so here is a quick fix
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+//main execution loop
+char user_input[100][80];
+int curr_idx =0;
+
+void history(){
+    int i=0;
+    while(strncmp(user_input[i],"\0", strlen(user_input[i]))){
+        printf("%d.", i+1);
+        yellow(user_input[i]);
+        printf("\n");
+        i++;
+    }
+}
+
+int launch(char command[30],char arg[50]){
+    //exit
+    if(!strcmp(command,"exit")){
+        return 0;
+    }
+    //echo
+    else if (!strcmp(command,"echo")){
+        printf("%s\n",arg);
+        return 1;
+    }
+    //pwd
+    else if (!strcmp(command,"pwd")){
+        /*
+        char cwd[PATH_MAX];
+        if(getcwd(cwd,sizeof(cwd)) != NULL){
+           blue(cwd);
+           printf("\n");
+           return 1;   
+        }else{
+            red("Error: pwd didnt work as expected!");
+            printf("\n");
+            return 1;
+        }*/
+        execl("/bin/pwd","/bin/pwd",NULL);
+        return 1;
+    }
+    //history
+    else if(!strcmp(command,"history")){
+        history();
+        return 1;
+    }
+    else{
+        printf("Command: \"%s\" not found.\n",command); 
+        return 1;
+    }
+    
+}
+void shell_loop(){
+    int status = 1;
+    char input[100];
+    char command[30];
+    char arg[50];
+    do{
+        cyan("assignment2@shell:");
+        magenta("~$ ");
+        fgets(input,100,stdin);
+        input[strcspn(input,"\n")] = 0;
+        if (!strcmp(input,"")){continue;}
+        strncpy(user_input[curr_idx], input, 80); 
+        curr_idx++;
+        //fgets(arg,50,stdin);
+        //printf("\n");
+        //printf("command:%s\narg:%s\n",command,arg);
+        //status = strcmp(command,"exit");
+        char *token;
+        token = strtok(input, " ");
+        strcpy(command, token);
+        token = strtok(NULL, "");
+        if(token != NULL) {
+            strcpy(arg, token);
+        } else {
+            strcpy(arg, "");
+        }        
+        status = launch(command,arg);
+    }while(status != 0);
+    
+}
---
+++ VERSION CONTROL/armaan/Assignment-2/Shellcommands/shell.h
@@ -0,0 +1,4 @@
+#include<stdio.h>
+#include<string.h>
+#include"Colors/color.h"
+void shell_loop();
