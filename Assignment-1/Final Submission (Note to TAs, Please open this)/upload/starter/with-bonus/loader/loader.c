dc6270a2d2b5daf2ac813aa8d60bc7957f3b0cc3
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-09-10 03:13:41 +0530

    Update loader.c

--- Assignment-1/Final Submission (Note to TAs, Please open this)/upload/starter/with-bonus/loader/loader.c
+++ Assignment-1/Final Submission (Note to TAs, Please open this)/upload/starter/with-bonus/loader/loader.c
@@ -1,102 +1,453 @@
-#include "loader.h"
+#include "shell.h"
 
-Elf32_Ehdr *ehdr;
-Elf32_Phdr *phdr;
-int fd;
+#define PATH_MAX 4096
 
-/*
- * release memory and other cleanups
- */
-void loader_cleanup() {
-  free(ehdr);
-  free(phdr);
+char user_input[100][80];
+int curr_idx =0;
+char exit_sequence[100][2000];
 
-  if(close(fd) == -1){
-    perror("ERROR");
-    exit(1);
-  };
+void Escape_sequence(int signum){
+    if(signum == SIGINT){
+        int i=0;
+        write(1,"\n",1);
+        while(strncmp(exit_sequence[i],"\0", strlen(exit_sequence[i]))){
+            int len = strlen(exit_sequence[i]);
+            for (int j = 0; j < len; j++) {
+                write(1, &exit_sequence[i][j], 1);
+            }
+            i++;
+        }
+        _exit(0);
+    }
+
+    if(signum == SIGCHLD){
+        int* cstatus = 0;
+        waitpid(-1,cstatus,WNOHANG);
+    }
 }
 
-/*
- * Load and run the ELF executable file
- */
-void load_and_run_elf(char** exe) {
-  if((fd = open(*exe, O_RDONLY))==-1){
-    perror("ERROR");
-    exit(1);
-  };
+char* trim(char* string, char* str){
+    int entry_status = 1;
+    int exit_status = 0;
+    int index = 0;
+    for(int i = 0; i < (int)strlen(string);i++){
+        if(entry_status == 1 && string[i] != ' ' && string[i] != '\t' && string[i] != '\n'){
+            entry_status = 0;
+            exit_status = 1;
+            str[index] = string[i];
+            index++;
+            continue;
+        }
+        if(exit_status == 1){
+            if(string[i] == ' ' || string[i] == '\t' || string[i] == '\n'){
+                int flag = 0;
+                for(int j = i; j < (int)strlen(string); j++){
+                    if(string[j] == ' ' || string[j] == '\t' || string[j] == '\n'){
+                        continue;
+                    }
+                    else{
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 0){
+                    str[index] = '\0';
+                    return str;
+                }
+                else{
+                    str[index] = string[i];
+                    index++;
+                    continue;
+                }
+            }
+            else if(string[i] == '\0'){
+                str[index] = '\0';
+                return str;
+            }
+            else{
+                str[index] = string[i];
+                index++;
+                continue;
+            }
+        }
+    }
 
-  // 1. Load entire binary content into the memory from the ELF file.
+    str[index] = '\0';
+    return str;
+}
 
-  ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr)); // For allocating memory for ELF Header
-  ssize_t NBytes_Read_EHDR = read(fd, ehdr, sizeof(Elf32_Ehdr));
-  if(NBytes_Read_EHDR == -1){
-    perror("ERROR");
-    exit(1);
+int lastBack(char* string){
+  int len = (int)strlen(string);
+  for(int i = len-1; i >=0; i--){
+    if(string[i] == ' ' || string[i] == '\t' || string[i] == '\n'){
+      continue;
+    }
+    else if(string[i] == '&'){
+      return 1;
+    }
+    else{
+      return 0;
+    }
   }
+  return 0;
+}
 
-  phdr = (Elf32_Phdr*)malloc((size_t)(ehdr->e_phentsize * ehdr->e_phnum)); // For allocating memory for Program Header Table
-  ssize_t NBytes_Read_PHDR = read(fd, phdr, (size_t)(ehdr->e_phentsize * ehdr->e_phnum));
-  if(NBytes_Read_PHDR == -1){
-    perror("ERROR");
-    exit(1);
-  }
+void remAmp(char* old_str, char* new_str){
+  int pos;
 
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-  //    type that contains the address of the entrypoint method in fib.c
-
-  Elf32_Phdr Req_Prog_Header;
-  int index;
-  int flag = 0;
-
-  for(int i = 0; i < ehdr->e_phnum; i++){
-    if(phdr[i].p_type == PT_LOAD && phdr[i].p_vaddr < ehdr->e_entry && phdr[i].p_vaddr + phdr[i].p_memsz > ehdr->e_entry){
-        Req_Prog_Header = phdr[i];
-        index = i;
-        flag = 1;
-        break; // break when we know that the segment is of type PT_LOAD and when we know that entrypoint lies in this segment
+  int len = (int)strlen(old_str);
+  for(int i = len-1; i >=0; i--){
+    if(old_str[i] == '&'){
+      pos = i;
+      break;
     }
   }
 
-  if(flag == 0){
-    printf("PT_LOAD Not Found.\n");
-    exit(1);
+  int i;
+
+  for(i = 0; i < pos; i++){
+    new_str[i] = old_str[i];
   }
 
-  // 3. Allocate memory of the size "p_memsz" using mmap function 
-  //    and then copy the segment content
+  new_str[i] = '\0';
+}
 
-  void* virtual_mem = mmap(NULL, Req_Prog_Header.p_memsz, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, 0,0);
-  if(virtual_mem == (void*)(-1)){
-    printf("MMAP ERROR\n");
-    exit(1);
-  }
+void history(){
+    int i=0;
+    while(strncmp(user_input[i],"\0", strlen(user_input[i]))){
+        printf("%d. ", i+1);
+        cyan(user_input[i]);
+        printf("\n");
+        i++;
+    }
+}
 
-  if(lseek(fd,phdr[index].p_offset,SEEK_SET) == -1){
-    perror("ERROR");
-    exit(1);
-  };
+int launch(char command[30],char arg[50],int mode){
+    int status = fork();
 
-  if(read(fd, virtual_mem,phdr[index].p_memsz) == -1){
-    perror("ERROR");
-    exit(1);
-  };
+    if(status < 0){
+        printf("Fork Failed");
+        return 1;
+    }
 
-  // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+    else if(status == 0){
+        if(mode == 0 || mode == 3){
+            char* args[50];
+            char str[50];
+            char* str2 = (char*)malloc(50*sizeof(char));
+            char* token = strtok(command,"|");
 
-  void* address =  virtual_mem + (ehdr->e_entry - phdr[index].p_vaddr);
+            int fd[2];
+            int tmp = 0;
 
-  // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+            int i = 0;
+            while(token != NULL){
+                trim(token,str);
+                strcpy(str2,str);
+                args[i] = str2;
+                str2 = str;
+                str2 = (char*)malloc(50*sizeof(char));
+                i++;
+                token = strtok(NULL,"|");
+            }
+            args[i] = NULL;
 
-  int (*_start)() = (int(*)())(address);
+            for(int j = 0; j < i; j++){
+                pipe(fd);
 
-  // 6. Call the "_start" method and print the value returned from the "_start"
+                int pid = fork();
+                if(pid == 0){
+                    dup2(tmp, 0);
+                    if(j != i-1){
+                        dup2(fd[1], 1);
+                    }
+                    close(fd[0]);
 
-  int result = _start();
-  printf("User _start return value = %d\n",result);
+                    char arg3[50];
+                    char *token1;
+                    token1 = strtok(args[j], " ");
+                    strcpy(command, token1);
+                    token1 = strtok(NULL, "");
+                    if(token1 != NULL) {
+                        strcpy(arg3, token1);
+                    }
+                    else {
+                        strcpy(arg3, "");
+                    }
 
-  if(munmap(virtual_mem,Req_Prog_Header.p_memsz) == -1){
-    perror("ERROR");
-    exit(1);
-  };
+                    char* arg2[50];
+                    token1 = strtok(arg3," ");
+                    arg2[0] = args[j];
+                    int i = 1;
+                    while(token1 != NULL){
+                        arg2[i] = token1;
+                        i++;
+                        token1 = strtok(NULL," ");
+                    }
+                    arg2[i] = NULL;
+
+                    execvp(args[j],arg2);
+                }
+                else{
+                    wait(NULL);
+                    close(fd[1]);
+                    tmp = fd[0];
+                }
+            }
+
+            exit(0);
+        }
+
+        if(!strcmp(command,"history")){
+            history();
+            exit(0);
+        }
+
+        char* main_str = (char*)malloc(100);
+        char* str1 = "which ";
+        char* str2 = " > /dev/null 2>.1";
+        int curr_index = 0;
+        for(int i = 0; i < (int)strlen(str1); i++){
+            curr_index++;
+            main_str[i] = str1[i];
+        }
+        for(int i = 0; i < (int)strlen(command); i++){
+            main_str[curr_index] = command[i];
+            curr_index++;
+        }
+        for(int i = 0; i < (int)strlen(str2); i++){
+            main_str[curr_index] = str2[i];
+            curr_index++;
+        }
+        if(system(main_str)){
+            printf("Command: \"%s\" not found.\n",command); 
+        }
+        else{
+            char* args[50];
+            char *token = strtok(arg," ");
+            args[0] = command;
+            int i =1;
+            while(token != NULL){
+                args[i] = token;
+                i++;
+                token = strtok(NULL," ");
+            }
+            args[i] = NULL;
+            execvp(command,args);
+        }
+        
+        exit(0);
+    }
+
+    else{
+        int ret;
+
+        if(mode != 2 && mode != 3){
+            status = wait(&ret);
+
+            if(!WIFEXITED(ret)) {
+                printf("Abnormal termination of %d\n",status);
+            }
+        }
+
+        return status;
+    }
+}
+
+void shell_loop(){
+    int status = 1;
+    char input[100];
+    char command[30];
+    char arg[50];
+
+    if(signal(SIGINT,Escape_sequence) == SIG_ERR){
+        perror("ERROR");
+        exit(1);
+    }
+
+    if(signal(SIGCHLD,Escape_sequence) == SIG_ERR){
+        perror("ERROR");
+        exit(1);
+    }
+
+    do{
+        char cwd[PATH_MAX];
+        if(getcwd(cwd,sizeof(cwd)) == NULL){
+            perror("ERROR");
+            exit(1);
+        }
+        magenta("assignment2@shell:");
+        cyan("~");
+        yellow(cwd);
+        white("$ ");
+
+        if(fgets(input,100,stdin) == NULL){
+            printf("fgets has failed or there is nothing to input anymore!");
+            exit(1);
+        }
+
+        input[strcspn(input,"\n")] = 0;
+        if (!strcmp(input,"")){continue;}
+
+        strncpy(user_input[curr_idx], input, 80); 
+
+        char test_input[100];
+        int flag_bg_detect = 0;
+        if(lastBack(input) == 1){
+            remAmp(input,test_input);
+            flag_bg_detect = 1;
+            strcpy(input,test_input);
+        }
+
+        if(strstr(input,"|")==NULL){
+            char *token;
+            token = strtok(input, " ");
+            strcpy(command, token);
+            token = strtok(NULL, "");
+            if(token != NULL) {
+                strcpy(arg, token);
+            }
+            else {
+                strcpy(arg, "");
+            }
+
+            if(!strcmp(command,"run")){
+                FILE *fptr;
+
+                fptr = fopen(arg, "r");
+                if(fptr == NULL){
+                    perror("ERROR");
+                    continue;
+                }
+
+                while(fgets(input, 100, fptr)) {
+                    input[strcspn(input,"\n")] = 0;
+
+                    if (!strcmp(input,"") || !strcmp(input,"\n")){continue;}
+
+                    strncpy(user_input[curr_idx], input, 80); 
+                    curr_idx++;
+
+                    if(strstr(input,"|")==NULL){
+                        char *token;
+                        token = strtok(input, " ");
+                        strcpy(command, token);
+                        token = strtok(NULL, "");
+                        if(token != NULL) {
+                            strcpy(arg, token);
+                        }
+                        else {
+                            strcpy(arg, "");
+                        }
+
+                        struct timeval start, end;
+                        time_t t,u;
+                        struct tm* info1;
+                        struct tm* info2;
+                        char buffer[64], buffer2[64];
+
+                        gettimeofday(&start, NULL);
+                        t = start.tv_sec;
+                        info1 = localtime(&t);
+                        strftime(buffer, sizeof buffer, "%A, %B %d - %H:%M:%S\n", info1);
+
+                        status = launch(command,arg,1);
+                        
+                        gettimeofday(&end, NULL);
+                        u = end.tv_sec;
+                        info2 = localtime(&u);
+                        strftime(buffer2, sizeof buffer2, "%A, %B %d - %H:%M:%S\n", info2);
+
+                        double duration = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000.0;
+                        sprintf(exit_sequence[curr_idx-1], "%d) Command \"%s\" executed by \n\tpid: %d\n\n\tStartTime: %s\n\tEndTime: %s\n\tDuration: %lf s\n\n",curr_idx, user_input[curr_idx-1], status, buffer, buffer2, duration);
+                    }
+
+                    else{
+                        struct timeval start, end;
+                        time_t t,u;
+                        struct tm* info1;
+                        struct tm* info2;
+                        char buffer[64], buffer2[64];
+
+                        gettimeofday(&start, NULL);
+                        t = start.tv_sec;
+                        info1 = localtime(&t);
+                        strftime(buffer, sizeof buffer, "%A, %B %d - %H:%M:%S\n", info1);
+
+                        status = launch(input,arg,0);
+                        
+                        gettimeofday(&end, NULL);
+                        u = end.tv_sec;
+                        info2 = localtime(&u);
+                        strftime(buffer2, sizeof buffer2, "%A, %B %d - %H:%M:%S\n", info2);
+
+                        double duration = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000.0;
+                        sprintf(exit_sequence[curr_idx-1], "%d) Command \"%s\" executed by \n\tpid: %d\n\n\tStartTime: %s\n\tEndTime: %s\n\tDuration: %lf s\n\n", curr_idx, user_input[curr_idx-1], status, buffer, buffer2, duration);
+                    }
+                    
+                }
+            
+                fclose(fptr);
+            }
+
+            else{
+                curr_idx++;
+                struct timeval start, end;
+                time_t t,u;
+                struct tm* info1;
+                struct tm* info2;
+                char buffer[64], buffer2[64];
+
+                gettimeofday(&start, NULL);
+                t = start.tv_sec;
+                info1 = localtime(&t);
+                strftime(buffer, sizeof buffer, "%A, %B %d - %H:%M:%S\n", info1);
+
+                if(flag_bg_detect == 1){
+                    status = launch(command,arg,2);
+                }
+                else{
+                    status = launch(command,arg,1);
+                }
+
+                gettimeofday(&end, NULL);
+                u = end.tv_sec;
+                info2 = localtime(&u);
+                strftime(buffer2, sizeof buffer2, "%A, %B %d - %H:%M:%S\n", info2);
+
+                double duration = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000.0;
+                sprintf(exit_sequence[curr_idx-1], "%d) Command \"%s\" executed by \n\tpid: %d\n\n\tStartTime: %s\n\tEndTime: %s\n\tDuration: %lf s\n\n",curr_idx, user_input[curr_idx-1], status, buffer, buffer2, duration);
+            }
+        }
+        else{
+            curr_idx++;
+            struct timeval start, end;
+            time_t t,u;
+            struct tm* info1;
+            struct tm* info2;
+            char buffer[64], buffer2[64];
+
+            gettimeofday(&start, NULL);
+            t = start.tv_sec;
+            info1 = localtime(&t);
+            strftime(buffer, sizeof buffer, "%A, %B %d - %H:%M:%S\n", info1);
+
+            if(flag_bg_detect == 1){
+                status = launch(input,arg,3);
+            }
+            else{
+                status = launch(input,arg,0);
+            }
+            
+            gettimeofday(&end, NULL);
+            u = end.tv_sec;
+            info2 = localtime(&u);
+            strftime(buffer2, sizeof buffer2, "%A, %B %d - %H:%M:%S\n", info2);
+
+            double duration = ((end.tv_sec * 1000000 + end.tv_usec) - (start.tv_sec * 1000000 + start.tv_usec))/1000000.0;
+            sprintf(exit_sequence[curr_idx-1], "%d) Command \"%s\" executed by \n\tpid: %d\n\n\tStartTime: %s\n\tEndTime: %s\n\tDuration: %lf s\n\n",curr_idx, user_input[curr_idx-1], status, buffer, buffer2, duration);
+        }
+    }
+    
+    while(status != 0);
 }
