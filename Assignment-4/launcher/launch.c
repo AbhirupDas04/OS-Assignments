97739166c04f308a668393417b10e1684286c4ba
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-10-31 23:46:50 +0530

    Error Handling Done

--- Assignment-4/launcher/launch.c
+++ Assignment-4/launcher/launch.c
@@ -1,51 +1,55 @@
 #include "loader.h"
 
-int main(int argc, char** argv) 
-{
-  if(argc != 2) {
-    printf("Usage: %s <ELF Executable> \n",argv[0]);
-    exit(1);
-  }
-  // 1. carry out necessary checks on the input ELF file
+int main(int argc, char** argv) {
+    if(argc != 2) {
+        printf("Usage: %s <ELF Executable> \n",argv[0]);
+        exit(1);
+    }
+    // 1. carry out necessary checks on the input ELF file
 
-  int fd;
+    int fd;
 
-  if((fd = open(argv[1], O_RDONLY))==-1){
-    perror("ERROR");
-    exit(1);
-  };
+    if((fd = open(argv[1], O_RDONLY))==-1){
+        perror("ERROR");
+        exit(1);
+    };
 
-  Elf32_Ehdr* ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr));
-  ssize_t NBytes_Read_EHDR = read(fd, ehdr, sizeof(Elf32_Ehdr));
-  if(NBytes_Read_EHDR == -1){
-    perror("ERROR");
-    exit(1);
-  }
+    Elf32_Ehdr* ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr));
+    if(ehdr == NULL){
+        printf("Malloc Failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    
+    ssize_t NBytes_Read_EHDR = read(fd, ehdr, sizeof(Elf32_Ehdr));
+    if(NBytes_Read_EHDR == -1){
+        perror("ERROR");
+        exit(1);
+    }
 
-  if(ehdr->e_ident[0] != ELFMAG0 && ehdr->e_ident[1] != ELFMAG1 && ehdr->e_ident[2] != ELFMAG2 && ehdr->e_ident[3] != ELFMAG3){
-    printf("Input file is not an ELF File.\n");
-    exit(1);
-  }
+    if(ehdr->e_ident[0] != ELFMAG0 && ehdr->e_ident[1] != ELFMAG1 && ehdr->e_ident[2] != ELFMAG2 && ehdr->e_ident[3] != ELFMAG3){
+        printf("Input file is not an ELF File.\n");
+        exit(1);
+    }
 
-  if(ehdr->e_type != ET_EXEC){
-    printf("Input file not an Executable file\n");
-    exit(1);
-  }
+    if(ehdr->e_type != ET_EXEC){
+        printf("Input file not an Executable file\n");
+        exit(1);
+    }
 
-  if(ehdr->e_version == EV_NONE){
-    printf("Input ELF file does not have the right Version\n");
-    exit(1);
-  }
+    if(ehdr->e_version == EV_NONE){
+        printf("Input ELF file does not have the right Version\n");
+        exit(1);
+    }
 
-  if(close(fd) == -1){
-    perror("ERROR");
-    exit(1);
-  };
+    if(close(fd) == -1){
+        perror("ERROR");
+        exit(1);
+    };
 
-  free(ehdr);
-  // 2. passing it to the loader for carrying out the loading/execution
-  load_and_run_elf(&argv[1]);
-  // 3. invoke the cleanup routine inside the loader  
-  loader_cleanup();
-  return 0;
+    free(ehdr);
+    // 2. passing it to the loader for carrying out the loading/execution
+    load_and_run_elf(&argv[1]);
+    // 3. invoke the cleanup routine inside the loader  
+    loader_cleanup();
+    return 0;
 }
--- Assignment-4/loader/loader.c
+++ Assignment-4/loader/loader.c
@@ -1,53 +1,86 @@
 #include "loader.h"
 
-Elf32_Ehdr *ehdr;
 Elf32_Phdr *phdr;
+Elf32_Ehdr* ehdr;
+void* virtual_mem;
 int fd;
-int index2;
-void* address;
 int page_size;
-Elf32_Phdr Req_Prog_Header;
-int count = 0;
-int curr_page_size;
+
+int n_page_faults;
+int n_page_allocs;
+int total_frag;
+
+int addr_array[1000];
+int addr_idx = 0;
 
 void Escape_sequence(int signum, siginfo_t *info){
     if(signum == SIGSEGV){
-      lseek(fd,0,SEEK_SET);
-    //   printf("%p\n",info->si_addr);
-      // if(munmap(virtual_mem,curr_page_size) == -1){
-      //   printf("aunt dead\n");
-      // }
-      for(int i = 0; i < ehdr->e_phnum; i++){
-        if(phdr[i].p_type == PT_LOAD && (void*)phdr[i].p_vaddr <= info->si_addr && (void*)phdr[i].p_vaddr + phdr[i].p_memsz >= info->si_addr){
-          // printf("%p\n",phdr[i].p_vaddr);
-            Req_Prog_Header = phdr[i];
-            index2 = i;
-            break; // break when we know that entrypoint lies in this segment
+        int index2;
+        Elf32_Phdr Req_Prog_Header;
+
+        n_page_faults++;
+
+        if(lseek(fd, 0, SEEK_SET) == -1){
+            if(write(1,"Lseek Failed!\n",14)){
+                exit(EXIT_FAILURE);
+            }
+            exit(EXIT_FAILURE);
         }
-      }
 
-      void* start_page = info->si_addr - (int)info->si_addr % page_size;
+        int flag = 0;
+        for(int i = 0; i < ehdr->e_phnum; i++){
+            if((void*)phdr[i].p_vaddr <= info->si_addr && (void*)phdr[i].p_vaddr + phdr[i].p_memsz >= info->si_addr){
+                flag = 1;
+                Req_Prog_Header = phdr[i];
+                index2 = i;
+                break; // break when we know that the segfault lies in this segment
+            }
+        }
 
-      int offset = start_page - (void*)phdr[index2].p_vaddr;
+        if(flag == 0){
+            if(write(1,"Seg Fault not occurring in any segment!\n",28) == -1){
+                exit(EXIT_FAILURE);
+            }
+            exit(EXIT_FAILURE);
+        }
 
-      //void* virtual_mem =  mmap((void*)phdr[index2].p_vaddr, curr_page_size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_FIXED, fd, phdr[index2].p_offset);
-      void* virtual_mem =  mmap((void*)phdr[index2].p_vaddr + offset,page_size,PROT_EXEC | PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED,-1,0);
-      
-      lseek(fd,phdr[index2].p_offset + offset,SEEK_SET);
+        void* start_page = info->si_addr - (int)info->si_addr % page_size;
+        int offset = start_page - (void*)phdr[index2].p_vaddr;
 
-      int n_bytes_read;
-      if(phdr[index2].p_memsz - offset < page_size){
-        n_bytes_read = phdr[index2].p_memsz - offset;
-      }
-      else{
-        n_bytes_read = page_size;
-      }
-    //   printf("%d %d\n",n_bytes_read , phdr[index2].p_memsz);
-      read(fd, virtual_mem,n_bytes_read);
-      
-      if(virtual_mem == NULL){
-        printf("aunt\n");
-      }
+        virtual_mem =  mmap((void*)phdr[index2].p_vaddr + offset,page_size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0); // fd = -1 because MAP_ANONYMOUS is present
+        if(virtual_mem == (void*)-1){
+            if(write(1,"MMAP Failed!\n",13)){
+                exit(EXIT_FAILURE);
+            }
+            exit(EXIT_FAILURE);
+        }
+
+        addr_array[addr_idx++] = (int)((void*)phdr[index2].p_vaddr + offset);
+        n_page_allocs ++;
+
+        if(lseek(fd,phdr[index2].p_offset + offset,SEEK_SET) == -1){
+            if(write(1,"Lseek Failed!\n",14)){
+                exit(EXIT_FAILURE);
+            }
+            exit(EXIT_FAILURE);
+        }
+
+        int n_bytes_read;
+        if(phdr[index2].p_memsz - offset < page_size){
+            n_bytes_read = phdr[index2].p_memsz - offset;
+        }
+        else{
+            n_bytes_read = page_size;
+        }
+
+        if(read(fd, virtual_mem,n_bytes_read) == -1){
+            if(write(1,"Read Failed!\n",13)){
+                exit(EXIT_FAILURE);
+            }
+            exit(EXIT_FAILURE);
+        }
+
+        total_frag += (page_size - n_bytes_read);
     }
 }
 
@@ -55,89 +88,86 @@ void Escape_sequence(int signum, siginfo_t *info){
  * release memory and other cleanups
  */
 void loader_cleanup() {
-  free(ehdr);
-  free(phdr);
+    for(int i = 0; i < addr_idx; i++){
+        if(munmap((void*)addr_array[i], page_size) == -1){
+            perror("MUNMAP");
+            exit(0);
+        }
+    }
 
-  if(close(fd) == -1){
-    perror("ERROR");
-    exit(1);
-  };
+    free(ehdr);
+    free(phdr);
+
+    if(close(fd) == -1){
+        perror("ERROR");
+        exit(1);
+    };
 }
 
 /*
  * Load and run the ELF executable file
  */
 void load_and_run_elf(char** exe) {
-  if((fd = open(*exe, O_RDONLY))==-1){
-    perror("ERROR");
-    exit(1);
-  };
+    if((fd = open(*exe, O_RDONLY))==-1){
+        perror("ERROR");
+        exit(1);
+    };
 
-  struct sigaction sig_struct;
-  memset(&sig_struct, 0, sizeof(sig_struct));
-  sig_struct.sa_handler = (void (*)(int))Escape_sequence;
-  sig_struct.sa_flags = SA_SIGINFO;
-  sigaction(SIGSEGV, &sig_struct, NULL);
+    struct sigaction sig_struct;
+    memset(&sig_struct, 0, sizeof(sig_struct));
+    sig_struct.sa_handler = (void (*)(int))Escape_sequence;
+    sig_struct.sa_flags = SA_SIGINFO;
+    if(sigaction(SIGSEGV, &sig_struct, NULL) == -1){
+        perror("SIGACTION");
+        exit(EXIT_FAILURE);
+    }
 
-  page_size = getpagesize();
+    page_size = getpagesize();
 
-  // 1. Load entire binary content into the memory from the ELF file.
+    // 1. Load entire binary content into the memory from the ELF file.
 
-  ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr)); // For allocating memory for ELF Header
-  ssize_t NBytes_Read_EHDR = read(fd, ehdr, sizeof(Elf32_Ehdr));
-  if(NBytes_Read_EHDR == -1){
-    perror("ERROR");
-    exit(1);
-  }
+    ehdr = (Elf32_Ehdr*)malloc(sizeof(Elf32_Ehdr)); // For allocating memory for ELF Header
+    if(ehdr == NULL){
+        printf("Malloc Failed!\n");
+        exit(EXIT_FAILURE);
+    }
 
-  phdr = (Elf32_Phdr*)malloc((size_t)(ehdr->e_phentsize * ehdr->e_phnum)); // For allocating memory for Program Header Table
-  ssize_t NBytes_Read_PHDR = read(fd, phdr, (size_t)(ehdr->e_phentsize * ehdr->e_phnum));
-  if(NBytes_Read_PHDR == -1){
-    perror("ERROR");
-    exit(1);
-  }
+    ssize_t NBytes_Read_EHDR = read(fd, ehdr, sizeof(Elf32_Ehdr));
+    if(NBytes_Read_EHDR == -1){
+        perror("ERROR");
+        exit(1);
+    }
 
-  // 2. Iterate through the PHDR table and find the section of PT_LOAD 
-  //    type that contains the address of the entrypoint method in fib.c
 
-  // int flag = 0;
+    phdr = (Elf32_Phdr*)malloc((size_t)(ehdr->e_phentsize * ehdr->e_phnum)); // For allocating memory for Program Header Table
+    if(phdr == NULL){
+        printf("Malloc Failed!\n");
+        exit(EXIT_FAILURE);
+    }
 
-  // for(int i = 0; i < ehdr->e_phnum; i++){
-  //   if(phdr[i].p_type == PT_LOAD && phdr[i].p_vaddr < ehdr->e_entry && phdr[i].p_vaddr + phdr[i].p_memsz > ehdr->e_entry){
-  //       Req_Prog_Header = phdr[i];
-  //       index2 = i;
-  //       flag = 1;
-  //       break; // break when we know that the segment is of type PT_LOAD and when we know that entrypoint lies in this segment
-  //   }
-  // }
+    ssize_t NBytes_Read_PHDR = read(fd, phdr, (size_t)(ehdr->e_phentsize * ehdr->e_phnum));
+    if(NBytes_Read_PHDR == -1){
+        perror("ERROR");
+        exit(1);
+    }
 
-  // if(flag == 0){
-  //   printf("PT_LOAD Not Found.\n");
-  //   exit(1);
-  // }
+    // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
 
-  // 3. Allocate memory of the size "p_memsz" using mmap function 
-  //    and then copy the segment content
+    void* address =  (void*)ehdr->e_entry;
 
-  // void* virtual_mem =  mmap((void*)phdr[index2].p_vaddr,page_size,PROT_EXEC | PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED,-1,0);
-  // lseek(fd,phdr[index2].p_offset,SEEK_SET);
-  // read(fd, virtual_mem,phdr[index2].p_memsz);
-  // 4. Navigate to the entrypoint address into the segment loaded in the memory in above step
+    // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
 
-  // void* address =  virtual_mem + (ehdr->e_entry - phdr[index2].p_vaddr);
-  void* address =  (void*)ehdr->e_entry;
+    int (*_start)() = (int(*)())(address);
 
-  // 5. Typecast the address to that of function pointer matching "_start" method in fib.c.
+    // // 6. Call the "_start" method and print the value returned from the "_start"
 
-  int (*_start)() = (int(*)())(address);
+    int result = _start();
 
-  // // 6. Call the "_start" method and print the value returned from the "_start"
+    printf("\n\n\nUser _start return value = %d\n\n",result);
 
-  int result = _start();
-  printf("\n\n\nUser _start return value = %d\n",result);
+    double total_frag_double = total_frag / 1024.0;
 
-  // if(munmap(virtual_mem,Req_Prog_Header.p_memsz) == -1){
-  //   perror("ERROR");
-  //   exit(1);
-  // };
+    printf("Total number of page faults: %d\n",n_page_faults);
+    printf("Total number of page allocations: %d\n",n_page_allocs);
+    printf("Total amount of internal fragmentation: %.3f KB (Rounded off to 3 digits) = %d Bytes\n",total_frag_double,total_frag);
 }
