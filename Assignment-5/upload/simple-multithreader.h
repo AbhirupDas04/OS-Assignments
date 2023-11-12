dd34db85512f7d7cf4f89f486e0dfe8ef88aec87
AbhirupDas04 <119044046+AbhirupDas04@users.noreply.github.com>
2023-11-12 23:45:18 +0530

    Time Added

--- Assignment-5/upload/simple-multithreader.h
+++ Assignment-5/upload/simple-multithreader.h
@@ -4,9 +4,12 @@
 #include <stdlib.h>
 #include <cstring>
 #include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
 
-typedef struct{
-    int loop_iter1;
+// This Struct is to contain all the details regarding the lambda function and its parameters.
+typedef struct{ 
+    int loop_iter1; // This is to determine the outerloop no.of iterations.
     int low1;
     int low2;
     int high2;
@@ -14,43 +17,88 @@ typedef struct{
     std::function<void(int,int)> fn2;
 }Lambda_Converter;
 
+typedef struct{
+    struct timespec start;
+    struct timespec end;
+    double diff_ms;
+    int type;
+    int numThreads;
+}Time_Controller;
+
+Time_Controller timestruct_arr[20];
+int curr_tstruct_idx = -1;
+
 int user_main(int argc, char **argv);
 
 /* Demonstration on how to pass lambda as parameter.
  * "&&" means r-value reference. You may read about it online.
  */
 void demonstration(std::function<void()> && lambda) {
-  lambda();
+    lambda();
 }
 
 int main(int argc, char **argv) {
-  /* 
-   * Declaration of a sample C++ lambda function
-   * that captures variable 'x' by value and 'y'
-   * by reference. Global variables are by default
-   * captured by reference and are not to be supplied
-   * in the capture list. Only local variables must be 
-   * explicity captured if they are used inside lambda.
-   */
-  int x=5,y=1;
-  // Declaring a lambda expression that accepts void type parameter
-  auto /*name*/ lambda1 = /*capture list*/[/*by value*/ x, /*by reference*/ &y](void) {
-    /* Any changes to 'x' will throw compilation error as x is captured by value */
-    y = 5;
-    std::cout<<"====== Welcome to Assignment-"<<y<<" of the CSE231(A) ======\n";
-    /* you can have any number of statements inside this lambda body */
-  };
-  // Executing the lambda function
-  demonstration(lambda1); // the value of x is still 5, but the value of y is now 5
+    /* 
+    * Declaration of a sample C++ lambda function
+    * that captures variable 'x' by value and 'y'
+    * by reference. Global variables are by default
+    * captured by reference and are not to be supplied
+    * in the capture list. Only local variables must be 
+    * explicity captured if they are used inside lambda.
+    */
+    int x=5,y=1;
+    // Declaring a lambda expression that accepts void type parameter
+    auto /*name*/ lambda1 = /*capture list*/[/*by value*/ x, /*by reference*/ &y](void) {
+        /* Any changes to 'x' will throw compilation error as x is captured by value */
+        y = 5;
+        std::cout<<"====== Welcome to Assignment-"<<y<<" of the CSE231(A) ======\n";
+        /* you can have any number of statements inside this lambda body */
+    };
+    // Executing the lambda function
+    demonstration(lambda1); // the value of x is still 5, but the value of y is now 5
 
-  int rc = user_main(argc, argv);
- 
-  auto /*name*/ lambda2 = [/*nothing captured*/]() {
-    std::cout<<"====== Hope you enjoyed CSE231(A) ======\n";
-    /* you can have any number of statements inside this lambda body */
-  };
-  demonstration(lambda2);
-  return rc;
+
+
+
+
+    int rc = user_main(argc, argv);
+
+    auto /*name*/ lambda2 = [/*nothing captured*/]() {
+        std::cout<<"====== Hope you enjoyed CSE231(A) ======\n";
+        /* you can have any number of statements inside this lambda body */
+    };
+    demonstration(lambda2);
+
+
+
+
+
+    if(curr_tstruct_idx >= 0){
+        printf("\n\nTime Taken by Each 'parallel_for' call:-\n\n");
+        for(int i = 0; i <= curr_tstruct_idx; i++){
+            printf("%d)\tType -> ", i + 1);
+            if(timestruct_arr[i].type == 1){
+                printf("Lambda Function with One Variable\n");
+            }
+            else{
+                printf("Lambda Function with Two Variables\n");
+            }
+
+            printf("\tNo. of Threads -> %d\n",timestruct_arr[i].numThreads);
+
+
+            char start_time_str[30], end_time_str[30];
+
+            strftime(start_time_str, sizeof(start_time_str), "%H:%M:%S", localtime(&timestruct_arr[i].start.tv_sec));
+            strftime(end_time_str, sizeof(end_time_str), "%H:%M:%S", localtime(&timestruct_arr[i].end.tv_sec));
+            printf("\tStart time -> %s.%3ld\n", start_time_str, timestruct_arr[i].start.tv_nsec);
+            printf("\tEnd time -> %s.%3ld\n", end_time_str, timestruct_arr[i].end.tv_nsec);
+            printf("\n\tTotal Time Taken -> %.3f seconds\n\n\n",timestruct_arr[i].diff_ms/1000);
+        }
+        printf("----------------------------------------------------------------------------\n");
+    }
+
+    return rc;
 }
 
 #define main user_main
@@ -78,6 +126,15 @@ void* fn_converter_2(void* converter) {
 // parallel_for accepts a C++11 lambda function and runs the loop body (lambda) in  
 // parallel by using ‘numThreads’ number of Pthreads to be created by the simple-multithreader 
 void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numThreads){
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    struct timespec sleep_time;
+    sleep_time.tv_sec = 4;
+    sleep_time.tv_nsec = 4 * 100000000;
+
+
+    
     int size = high - low;
     numThreads = numThreads - 1;
 
@@ -116,12 +173,36 @@ void parallel_for(int low, int high, std::function<void(int)> &&lambda, int numT
     for (int i=1; i <= numThreads; i++) {
         pthread_join(thread_id_arr[i-1] , NULL);
     }
+
+
+
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    long long diff_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+    double diff_ms = (double)diff_ns / 1000000.0;
+
+    Time_Controller t1;
+    t1.diff_ms = diff_ms;
+    t1.end = end;
+    t1.numThreads = numThreads;
+    t1.start = start;
+    t1.type = 1;
+    timestruct_arr[++curr_tstruct_idx] = t1;
 }
 
 // This version of parallel_for is for parallelizing two-dimensional for-loops, i.e., an outter for-i loop and  
 // an inner for-j loop. Loop properties, i.e. low, high are mentioned below for both outter  
 // and inner for-loops. The suffixes “1” and “2” represents outter and inner loop properties respectively.  
 void parallel_for(int low1, int high1,  int low2, int high2, std::function<void(int, int)>  &&lambda, int numThreads){
+    struct timespec start, end;
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    struct timespec sleep_time;
+    sleep_time.tv_sec = 4;
+    sleep_time.tv_nsec = 4 * 100000000;
+
+
+
     int size = high1 - low1;
     numThreads = numThreads - 1;
 
@@ -162,6 +243,19 @@ void parallel_for(int low1, int high1,  int low2, int high2, std::function<void(
     for (int i=1; i <= numThreads; i++) {
         pthread_join(thread_id_arr[i-1] , NULL);
     }
+
+
+
+    clock_gettime(CLOCK_REALTIME, &end);
+
+    long long diff_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
+    double diff_ms = (double)diff_ns / 1000000.0;
+
+    Time_Controller t1;
+    t1.diff_ms = diff_ms;
+    t1.end = end;
+    t1.numThreads = numThreads;
+    t1.start = start;
+    t1.type = 2;
+    timestruct_arr[++curr_tstruct_idx] = t1;
 }
-
-
